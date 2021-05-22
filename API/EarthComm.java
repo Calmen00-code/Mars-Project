e18a126df30e84c986aec5ee0649aa365198a0f4
calmen <calmenchia@gmail.com>
2021-05-22 15:38:44 +0800

    First Commit

---
+++ API/EarthComm.java
@@ -0,0 +1,20 @@
+public class EarthComm
+{
+    /** 
+     * Return the next command received from Earth, or null if no further command 
+     * has been received. If multiple commands arrive in between calls to 
+     * pollCommand(), they will be buffered (stored temporarily), and subsequent 
+     * calls to pollCommand() will return one command at a time, in the order of 
+     * arrival. 
+     *
+     * There is no risk of commands being lost. But pollCommand() *will not wait*
+     * for a command to be received if none has been yet.
+     */
+    public String pollCommand() 
+    {
+        return "Test pollCommand";
+    }
+
+    /** Sends a return message to Earth. */
+    public void sendMessage(String msg) {}
+}
---
+++ API/Sensors.java
@@ -0,0 +1,14 @@
+public class Sensors
+{
+    /** Performs a temperature reading and returns the result in °C. */
+    public double readTemperature() { return 1.0; }
+
+    /** Performs a visibility reading and returns the result in km. */
+    public double readVisibility() { return 1.0; }
+
+    /** Performs a light-level reading, and returns the result in lux (units). */
+    public double readLightLevel() { return 1.0; }
+
+    /** Takes a photo and returns the binary data making up the image. */
+    public byte[] takePhoto() { return null; }
+}
