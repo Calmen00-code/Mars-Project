c783648eea082c1fa646d661b63af139bfab13b6
calmen <calmenchia@gmail.com>
2021-05-25 07:10:46 +0800

    Updated package in State

---
+++ src/State/RoverContext.java
@@ -0,0 +1,60 @@
+/**
+ * Program: RoverContext.java
+ * Purpose: Class that handles the state pattern for the Rover
+ */
+
+package Assignment2.State;
+
+public class RoverContext
+{
+    private DriveState isDrive;
+    private IdleState isIdle;
+    private EngineSystem engine;
+/*  
+    FIXME 
+    private SensorOnState sensorOn;
+    private SensorIdleState sensorIdle;
+*/
+
+    private RoverState roverState;
+
+    public RoverContext()
+    {
+        // Initialising all states to this current object
+        isDrive = new DriveState(this);
+        isIdle = new IdleState(this);
+        engine = new EngineSystem();
+/*
+        FIXME
+        sensorOn = new SensorOnState(this);
+        sensorIdle = new SensorIdleState(this);
+*/
+
+        // Rover is not moving initially
+        roverState = isIdle;
+
+        // Rover is not reading any data initially
+        // FIXME sensorIdle = 
+    }
+
+    public void setRoverState( RoverState newRoverState )
+    {
+        roverState = newRoverState;
+    }
+
+    public void startDrive()
+    {
+        roverState.startDrive();
+        engine.startDriving();
+    }
+
+    public void stopDrive()
+    {
+        roverState.stopDrive();
+        engine.stopDriving();
+    }
+
+    public DriveState getDriveState() { return isDrive; }
+    public IdleState getIdleState() { return isIdle; }
+    public EngineSystem getEngine() { return engine; }
+}
---
+++ src/State/RoverState.java
@@ -0,0 +1,12 @@
+/**
+ * Program: RoverState.java
+ * Purpose: Allows Polymorphism on each state class idle and not idle
+ */
+
+package Assignment2.State;
+
+public interface RoverState
+{
+    public String startDrive();    
+    public String stopDrive();
+}
