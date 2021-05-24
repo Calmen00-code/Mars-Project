f8c0f085c03c971b450dd8be515c014c5c530ac4
calmen <calmenchia@gmail.com>
2021-05-24 22:51:11 +0800

    Added EngineEvent

--- Observer/EngineEvent.java
+++ Observer/EngineEvent.java
@@ -21,16 +21,36 @@ public class EngineEvent implements RoverObserver
     public String checkCommand( String command )
     {
         String msg = "", angle = "";
-        double distance = 0.0;
-        String[] parseCommand = command.split(" ");
+        double distance = 0.0, angle = 0.0;
 
-        if ( parseCommand[0].equals("D") ) {
+        String[] parseCommand = command.split(" "); // Parse the command
+
+        if ( parseCommand[0].equals("D") ) { // Checking for drive
             try {
+                // Second argument must be a valid distance
                 distance = Double.parseDouble(parseCommand[1]);
-                if ( distance <= 0 )
-                    msg += ""
+                if ( engine.getDistanceDrive() <= 0 ) {
+                    msg += "Rover has reached its destination";
+                    rover.stopDrive();
+                } else {
                     msg += "Drive for " + distance;
+                    rover.startDrive();
+                }
             } catch (Exception e) {
                 throw new EngineEventException("Invalid distance");
             }
-        } else if ( parseCommand[1].equals(""
+        } else if ( parseCommand[1].equals("T") ) { // Checking for turn
+            try {
+                // Second argument must be a valid angle
+                angle = Double.parseDouble(parseCommand[1]);
+                if ( angle >= -180 && angle <= 180 )
+                    msg += "Rover turn for " + angle + "degree";
+                else
+                    throw new EngineEventException("Invalid angle");
+            } catch (Exception e) {
+                throw new EngineEventException("Invalid angle");
+            }
+        }
+        return msg;
+    } 
+}
