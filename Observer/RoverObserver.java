85a9c1c9ced982db1413c02c5469d641544e2eb8
calmen <calmenchia@gmail.com>
2021-05-24 20:48:54 +0800

    Added RoverObserver

---
+++ Observer/RoverObserver.java
@@ -0,0 +1,9 @@
+/**
+ * Program: RoverObserver.java
+ * Purpose: Handling all the observers for the event of rover
+ */
+
+public interface RoverObserver
+{
+    public String checkCommand(String command);
+}
