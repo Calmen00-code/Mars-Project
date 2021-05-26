2a1feeece72acef3453670e0a1c9176b99cb8c83
calmen <calmenchia@gmail.com>
2021-05-26 11:47:34 +0800

    Backup before making changes on build.xml

--- Observer/RoverObserver.java
+++
@@ -1,9 +0,0 @@
-/**
- * Program: RoverObserver.java
- * Purpose: Handling all the observers for the event of rover
- */
-
-public interface RoverObserver
-{
-    public String checkCommand(String command);
-}
