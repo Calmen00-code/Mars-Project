f80462e5286de2e949ca2c1b2ee6faaf33353003
calmen <calmenchia@gmail.com>
2021-05-26 16:31:29 +0800

    Remove resources dir

---
+++ src/DemoTest/RoverTest.java
@@ -0,0 +1,30 @@
+/**
+ * Program: RoverDemo.java
+ * Purpose: Testing the value for the Rover Demonstration
+ */
+
+import java.util.*;
+import Assignment2.State.*;
+import Assignment2.Observer.*;
+import Assignment2.API.*;
+
+public class RoverTest
+{
+    public static void main(String[] args)
+    {
+        String output = "";
+        EngineSystem engine = new EngineSystem();
+        SoilAnalyser analyser = new SoilAnalyser();
+        RoverContext rover = new RoverContext( engine, analyser );
+
+        System.out.println("=========================================\n");
+        System.out.println("TEST Moving Successful: ");
+        try { output = rover.startDrive(); }
+        catch( Exception e ) { output = e.getMessage(); }
+        if ( output.equals("Rover is already moving") )
+            System.out.println("PASSED");
+        else
+            System.out.println("FAILED");
+        
+    }
+}
