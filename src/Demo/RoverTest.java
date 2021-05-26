05f87c607b765d1a4f73565ef37f1ab4ce9cae62
calmen <calmenchia@gmail.com>
2021-05-26 16:10:04 +0800

    LATEST

---
+++ src/Demo/RoverTest.java
@@ -0,0 +1,15 @@
+/**
+ * Program: RoverDemo.java
+ * Purpose: Testing the value for the Rover Demonstration
+ */
+
+public class RoverTest
+{
+    public static void main(String[] args)
+    {
+        System.out.println("=========================================\n");
+        System.out.println("TEST Moving Successful: ");
+        
+        
+    }
+}
