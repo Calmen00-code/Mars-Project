baac696a11faaa27167dfacdb072c05ef4e8a384
calmen <calmenchia@gmail.com>
2021-05-27 22:20:08 +0800

    Assigning values to the returned variables

--- src/API/Sensors.java
+++ src/API/Sensors.java
@@ -3,13 +3,13 @@ package Assignment2.API;
 public class Sensors
 {
     /** Performs a temperature reading and returns the result in °C. */
-    public double readTemperature() { return 1.0; }
+    public double readTemperature() { return -15.8; }
 
     /** Performs a visibility reading and returns the result in km. */
-    public double readVisibility() { return 1.0; }
+    public double readVisibility() { return 7.1; }
 
     /** Performs a light-level reading, and returns the result in lux (units). */
-    public double readLightLevel() { return 1.0; }
+    public double readLightLevel() { return 13774; }
 
     /** Takes a photo and returns the binary data making up the image. */
     public byte[] takePhoto() 
